1d4bbc19f62077108fdaa43906f25a34ee768c10
ilyas-doughmi <doughmi.projects@gmail.com>
2025-10-28 15:19:39 +0100

    fixing margin left

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const close_popup = document.getElementById("close_add_modal");
 const add_popup = document.getElementById("popup");
 const add_btn = document.getElementById("add_btn");
 
+const cancel_btn = document.getElementById("Cancel_btn");
+
 add_popup.hidden = true;
 
 add_btn.onclick = function(){
     add_popup.hidden = false;
 }
 
-
+cancel_btn.onclick = function(){
+    add_popup.hidden = true;
+}
 
 
 
