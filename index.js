aae2b1a715fd56a302a3181ac5a39158b3c2b8cd
ilyas-doughmi <doughmi.projects@gmail.com>
2025-10-29 12:49:18 +0100

    finishing front-end and change the transaction recent to cards instead of lists

--- index.js
+++ index.js
@@ -7,31 +7,57 @@ const tran_btn = document.getElementById("tran_btn");
 const cancel_btn = document.getElementById("Cancel_btn");
 const x_btn = document.getElementById("x_btn");
 const income_radio = document.getElementById("income_radio");
+const three_points = document.getElementById("three-points");
 
-add_popup.hidden = true;
 
-add_btn.onclick = function(){
+
+three_points.onclick = function () {
+    small_menu.style.opacity = 1000;
+}
+
+add_btn.onclick = function () {
     add_popup.hidden = false;
 }
 
-cancel_btn.onclick = function(){
+cancel_btn.onclick = function () {
     add_popup.hidden = true;
 }
 
-tran_btn.onclick = function(){
-    if(income_radio.checked){
-        window.alert("Hello")
-    }
-    else{
-        window.alert("fucku");
-    }
-}
 
-x_btn.onclick = function(){
-        add_popup.hidden = true;
+x_btn.onclick = function () {
+    add_popup.hidden = true;
 
 }
 
-total_exp.textContent = "30";
 
-total_inc.textContent = "100";
+
+// GOOD THINGS ARE DOWN HERE :)
+const amount = document.getElementById("amount");
+const description = document.getElementById("description");
+const container = document.querySelector(".container");
+
+function add() {
+
+    if (amount.value != "") {
+        if (income_radio.checked) {
+            let h1 = document.createElement("h1");
+            h1.innerHTML = "$" + amount.value;
+            h1.classList.add("text-green-600");
+            container.appendChild(h1);
+            add_popup.hidden = true;
+        }
+        else{
+            let h1 = document.createElement("h1");
+            h1.innerHTML = "$" + amount.value;
+            h1.classList.add("text-red-600");
+            container.appendChild(h1);
+            add_popup.hidden = true;
+        }
+
+
+    } else {
+        window.alert("Problem :(");
+    }
+}
+
+
